6bb109f7058cf563f9caf109cf3ec600528610d6
Zié Mohamed Coulibaly <zie.mc@hotmail.com>
2024-10-03 13:41:11 -0400

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import {
+	ClerkProvider,
+	SignInButton,
+	SignedIn,
+	SignedOut,
+	UserButton,
+} from "@clerk/nextjs";
+
+// Import Poppins font with the desired weights, styles, and subsets
+const poppins = Poppins({
+	weight: ["300", "400", "500", "600", "700"],
+	style: ["normal", "italic"],
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+	title: "Project",
+	description: "SaaS Project",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<ClerkProvider
+			appearance={{
+				variables: { colorPrimary: "#624cf5" },
+			}}
+		>
+			<html lang="en">
+				<head>
+					{/* Favicon link */}
+					<link rel="icon" href="/favicon.ico" />
+				</head>
+				<body className={cn("font-poppins antialiased", poppins.variable)}>
+					{/* <SignedOut>
+						<SignInButton />
+					</SignedOut>
+					<SignedIn>
+					</SignedIn> */}
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
+	);
+}
